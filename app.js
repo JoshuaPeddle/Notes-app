7e0905ccf717ea70755cc4f1951dfc760d173579
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-06 22:40:45 -0230

    Add auth.js router. Set up express-session.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var session = require('express-session');
+var passport = require('passport');
 var express = require('express');
 var path = require('path');
 require('dotenv').config({ path: './.env' })
@@ -10,16 +12,22 @@ var app = express();
 /* Import Routers */
 var noteRouter = require('./routes/notes.js');
 var indexRouter = require('./routes/index.js');
+var authRouter = require('./routes/auth.js');
 
-app.use(express.static(path.join(__dirname , 'view')));
-
-
+app.use(express.static(path.join(__dirname, 'view')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+app.use(passport.authenticate('session'))
 
 /* Use Routers */
 app.use('/', noteRouter);
 app.use('/', indexRouter);
+app.use('/', authRouter);
 
 module.exports = app;
