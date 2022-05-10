4b6e612b4889f041b806a91beb89e7d81b927140
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-10 12:23:18 -0230

    Testing with mocha and chai.

---
+++ tests/integration.js
@@ -0,0 +1,58 @@
+
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var expect = require('chai').expect;
+
+
+chai.use(chaiHttp)
+chai.should()
+
+
+
+describe('Notes app - Integration Tests with Mocha', function () {
+
+    /** Integration tests for Api calls */
+    describe('Test API calls - individual', function () {
+
+        it('GET / - Should return Login page', function (done) {
+            chai.request('http://localhost:3000')
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.type.should.equal('text/html');
+                    // Ensure page contains strings expected the login page
+                    res.text.should.have.string("login");
+                    res.text.should.have.string("signup");
+                    console.log("GET / done")  
+                    done()
+                   
+                });
+            
+        });
+
+        it('POST /signup - ', function (done) {
+            chai.request('http://localhost:3000')
+                .post('/signup')
+                .send({ "username": "12345678", "password": "test" })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.type.should.equal('text/html');
+                    console.log("POST /signup done")
+                    done()
+                });
+        });
+
+        it('DELETE /users - Delete a user', function (done) {
+            chai.request('http://localhost:3000')
+                .delete('/users')
+                .send({ "username": "12345678" })
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.text.should.be.oneOf(['User does not exist', 'OK']);
+                    done()   
+                });
+        });
+    });
+});
+
---
+++ tests/mocha-tests.js
@@ -0,0 +1,53 @@
+
+
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+
+chai.use(chaiHttp)
+chai.should()
+
+
+process.env.MONGODB_CONNSTRING = 'mongodb://localhost';
+process.env.DBNAME = 'notesapp-dev'
+
+const app = require('../app.js');
+
+
+
+describe('Notes app - Tests with Mocha', function () {
+
+
+
+    /** Tests for Api calls */
+    describe('Test API calls', function () {
+
+        it('GET /',  function (done) {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.type.should.equal('text/html');
+                    done()
+                });
+        });
+    });
+
+
+    /** Integration tests for Api calls */
+    describe('Test API calls', function () {
+
+        it('GET /',  function (done) {
+            chai.request(app)
+                .get('/')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.type.should.equal('text/html');
+                    done()
+                });
+        });
+    });
+
+
+
+
+});
