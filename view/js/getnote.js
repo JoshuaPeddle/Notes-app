e04d6249c38b11f5178c11ea630644dfdabbec20
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-11 22:19:04 -0230

    Visual changes to HTML & CSS. Add getnote.js

--- view/js/getnote.js
+++ view/js/getnote.js
@@ -16,11 +16,10 @@ $(function () {
 			type: 'get',
 			contentType: 'application/json',
 			success: function (response) {
-				// We can print in the front-end console to verify
-				// what is coming back from the server side
-				console.log(JSON.stringify(response));
-				alert(JSON.stringify(response));
-				//$("#add-out").text(response);
+				if (response === 'No notes found'){
+					return;
+				}
+				addNotesTo(response);
 			},
 			//We can use the alert box to show if there's an error in the server-side
 			error: function (xhr) {
@@ -30,3 +29,35 @@ $(function () {
 		});
 	});
 });
+
+
+function addNotesTo(response){
+	$('#all_notes').empty();
+	if (Array.isArray(response)){response.forEach(element => {
+		let $toAdd =constructNoteDiv(element['title'],element['body']);
+		$('#all_notes').append($toAdd);
+	});
+	}
+	else{
+		let $toAdd =constructNoteDiv(response['title'],response['body']);
+
+		$('#all_notes').append($toAdd);
+	}	
+}
+
+
+function constructNoteDiv(title, body){
+	if (title.length >10){
+		title = body.slice(0,10) +'...';
+	}
+	if (body.length >20){
+		body = body.slice(0,40) +'...';
+	}
+	let $div = $('<div>').attr('id','note_container');
+	let $title = $('<label>').text('Title: '+title.toString()).attr('id','note_title');
+	let $body = $('<label>').text('Body: '+body.toString()).attr('id','note_body');
+	
+	$title.appendTo($div);
+	$body.appendTo($div);
+	return $div;
+}
