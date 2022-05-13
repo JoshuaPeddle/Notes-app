73d976cb4c0da3db95743cd7ed214a0bbe0b0757
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-13 15:22:24 -0230

    Add check for DEV env

--- utils/cache.js
+++ utils/cache.js
@@ -2,22 +2,25 @@ var express = require('express');
 var router = express.Router();
 
 
+// Cache static CSS for a year
 router.get('/css/*', (req, res, next)=>{
 	res.set('Cache-control', 'public, max-age=31536000, immutable');
-	if(process.env.DEV){
-		res.set('Cache-control', 'no-cache');
-	}
 	next();
 });
 
-
+// Cache static JS for a year
 router.get('/js/*', (req, res, next)=>{
 	res.set('Cache-control', 'public, max-age=31536000, immutable');
-	if(process.env.DEV){
-		res.set('Cache-control', 'no-cache');
-	}
+
 	next();
 });
 
+// If we're testing, disable the cache
+if(process.env.DEV){
+	router.get('/*', (req, res, next)=>{
+		res.set('Cache-control', 'no-cache');
+		next();
+	});
+}
 
 module.exports = router;
