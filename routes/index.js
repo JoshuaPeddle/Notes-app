e931eb7dff7a4f87e22e7ffe897cd5b2d5a61b23
Joshua Peddle <37007232+JoshuaPeddle@users.noreply.github.com>
2022-05-14 21:38:43 -0230

    using static files. Change routes.

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,17 @@
 var express = require('express');
-var path = require('path');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res) {
 	res.set('Cache-control', 'no-cache');
 	if(req.user){  // If user has an active session skip login page
-		res.sendFile(path.join(__dirname, '..', 'view', 'notes.html'));
+		res.redirect('notes.html');
 	}else{   // No active session, go to login page
-		res.sendFile(path.join(__dirname, '..', 'view', 'login.html'));
+		res.redirect('login.html');
 	}
 });
+
+
+
 
 module.exports = router;
